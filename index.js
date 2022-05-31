b44200ebb1a31bc3d90f0e703d628a7497ac1ca3
Ignacio Yz <ignacioyz@gmail.com>
2022-05-31 13:26:43 -0400

    initial fetch lga

--- index.js
+++ index.js
@@ -3,14 +3,26 @@
 function getArrivalOrDeparture() {
     fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/arrivals?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
         .then((response) => response.json())
-        .then((arrivalData) => console.log(arrivalData))
+        .then((arrivalData) => (arrivalData))
         
         fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
         .then(response => response.json())
         .then(departureData => console.log(departureData))
     }
      
- getArrivalOrDeparture()
+let inputForm = document.getElementById('init-airport-form')
+let initialInput = document.getElementById('initial-input')
+let initialBtn = document.getElementById('initial-btn')
+
+
+getArrivalOrDeparture()
+
+
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+   
+})
 
 
    
