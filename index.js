057b69b04ec48aa2d232ceff8806cb5c30e59ea3
Ignacio Yz <ignacioyz@gmail.com>
2022-05-31 11:32:46 -0400

    initial-functins

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-fetch('https://raw.githubusercontent.com/zmsp/coronavirus-json-api/master/metadata.json')
-.then((response) => response.json())
-.then((json) => console.log(json))
+
+function getArrivalOrDeparture() {
+    fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/arrivals?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
+        .then((response) => response.json())
+        .then((arrivalData) => console.log(arrivalData))
+        
+        fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
+        .then(response => response.json())
+        .then(departureData => console.log(departureData))
+    }
+     
+ getArrivalOrDeparture()
+
+
+   
