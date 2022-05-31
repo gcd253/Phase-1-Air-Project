8415f684f44c330b5702b980a291626163db7e2e
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-31 13:15:35 -0400

    link for jfk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/arrivals?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
+fetch('https://api.flightstats.com/flex/fids/rest/v1/json/jfk/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
 .then((response) => response.json())
 .then((json) => console.log(json))
 
