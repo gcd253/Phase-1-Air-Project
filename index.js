4d4d51bb8d062fb3e57f72af5570d7255f46c0f8
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-31 16:09:22 -0400

    interpolated url added

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-function createJsonArray() {
 
-const flightData = []
+function airportLookUp() {
+    const form = document.getElementById('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
 
-fetch('https://api.flightstats.com/flex/fids/rest/v1/json/jfk/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&includeAirlines=dl%2Caa%2Cua&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
+        const formInput = document.getElementById('formInput')
+
+        const airportCode = formInput.value;
+
+
+        fetch(`https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&includeAirlines=dl%2Caa%2Cua&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false`)
 .then((response) => response.json())
-.then((jsonJfk) => flightData.push(jsonJfk))
-
-
-fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&includeAirlines=dl%2Caa%2Cua&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
-.then((response) => response.json())
-.then((jsonLga) => flightData.push(jsonLga))
-
-
-fetch('https://api.flightstats.com/flex/fids/rest/v1/json/ewr/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&includeAirlines=dl%2Caa%2Cua&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
-.then((response) => response.json())
-.then((jsonEwr) => flightData.push(jsonEwr))
-
-
-console.log(flightData)
-
+.then((jsonData) => console.log(jsonData))
+    
+    })
 }
 
-createJsonArray()
+airportLookUp()
