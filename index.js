f617207a7ec951cf2b9f066ae9757e37deb8523b
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-31 18:37:02 -0400

    created set to house city/airport code pairs

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ function airportLookUp() {
         const formInput = document.getElementById('initial-input')
         const airportCode = formInput.value;
         fetch(`https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false`)
-.then((response) => response.json())
-.then((jsonData) => console.log(jsonData))
+        .then((response) => response.json())
+        .then((jsonData) => console.log(jsonData))
+
+        
     })
 }
+const citiesAndCodes = new Set();
 
+function createCityCodeList() {
 
-// airportLookUp()
+fetch('https://api.flightstats.com/flex/fids/rest/v1/json/jfk/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=city%2CdestinationAirportCode&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
+.then((response) => response.json())
+.then(cityAndCodeJson => {
+    for(const item of cityAndCodeJson.fidsData)
+    citiesAndCodes.add(item.city + " - " + item.destinationAirportCode)
+    //console.log(citiesAndCodes)
+})
+
+}
+
+createCityCodeList()
+
+console.log(citiesAndCodes)
