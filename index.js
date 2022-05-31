731d45f241be5d4e372628b97fdf0d741c013a6a
Ignacio Yz <ignacioyz@gmail.com>
2022-05-31 16:53:46 -0400

    grabbing css

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
-function airportLookUp() {
+let backgroundColor = document.getElementsByTagName('body')
+body.innerHTML = backgroundColor;blue 
+
+function departureLookUp() {
     const form = document.getElementById('init-airport-form')
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         const formInput = document.getElementById('initial-input')
         const airportCode = formInput.value;
         fetch(`https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false`)
-.then((response) => response.json())
-.then((jsonData) => console.log(jsonData))
+            .then(response => response.json())
+            .then(departureData => console.log(departureData))
     })
 }
+departureLookUp()
 
-airportLookUp()
+function arrivalLookup(){
+    fetch(`curl -v  -X GET "https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/arrivals?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cgate%2Cremarks&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false"`)
+}
