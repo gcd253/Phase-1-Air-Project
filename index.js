d47eb6ec4819a396c47c18f3a955ae22cd2ecaa0
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-31 11:25:04 -0400

    form and header created

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 fetch('https://api.flightstats.com/flex/fids/rest/v1/json/lga/arrivals?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111')
 .then((response) => response.json())
 .then((json) => console.log(json))
+
