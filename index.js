18341af356a4d0fb1f93957e58894cc9b3db784a
Ignacio Yz <ignacioyz@gmail.com>
2022-06-02 16:13:59 -0400

    Brandon for President

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ function departureLookUp() {
             resultDiv.removeChild(resultDiv.firstChild);
         }
 
-        fetch(`https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false`)
+        // fetch(`https://api.flightstats.com/flex/fids/rest/v1/json/${airportCode}/departures?appId=9ca90017&appKey=0c3c79fb2ebeab31c3ad63ddc4282230&requestedFields=airlineCode%2CflightNumber%2Ccity%2CcurrentTime%2Cremarks%2CoriginAirportCode%2CdestinationAirportCode%2CairlineName%2CcurrentGateDate&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false`)
 
+        fetch('http://localhost:3000/fidsData')
             .then(response => response.json())
             .then(departureData => {
                 (departureData)
-                // console.log(departureData)
+                console.log(departureData)
 
                 allFlightData = departureData
 
-                for (let item of departureData.fidsData) {
+                for (let item of departureData) {
                     let orderedList = document.createElement('ol')
                     orderedList.className = item.airlineName
                     orderedList.textContent = item.airlineName + " " + item.flightNumber
@@ -79,13 +80,13 @@ const citiesAndCodes = new Set();
 
 function createCityCodeList() {
 
-    fetch('https://api.flightstats.com/flex/fids/rest/v1/json/jfk/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=city%2CdestinationAirportCode&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
-        .then((response) => response.json())
-        .then(cityAndCodeJson => {
-            for (const item of cityAndCodeJson.fidsData)
-                citiesAndCodes.add(item.city + " - " + item.destinationAirportCode)
-            //console.log(citiesAndCodes)
-        })
+    // fetch('https://api.flightstats.com/flex/fids/rest/v1/json/jfk/departures?appId=9ca90017&appKey=430095eb345366c16e16381b17c69111&requestedFields=city%2CdestinationAirportCode&lateMinutes=15&useRunwayTimes=false&excludeCargoOnlyFlights=false')
+    //     .then((response) => response.json())
+    //     .then(cityAndCodeJson => {
+    //         for (const item of cityAndCodeJson.fidsData)
+    //             citiesAndCodes.add(item.city + " - " + item.destinationAirportCode)
+    //         //console.log(citiesAndCodes)
+    //     })
 
 }
 
